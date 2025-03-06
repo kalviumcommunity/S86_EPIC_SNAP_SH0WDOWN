e267fbdc457ab397ebdd2e847ed0061d065e87fd
unknown <nallakukkala.supriya.s86@kalvium.community>
2025-03-06 10:40:02 +0530

    added files

--- server.js
+++ server.js
@@ -1,6 +1,30 @@
 const express = require("express");
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const authRoutes = require('./routes');
+require('dotenv').config();
+
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+
+// MongoDB connection
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.error('MongoDB connection error:', err));
+
+app.use(bodyParser.json());
+
+
+app.use('/api', authRoutes);
+
+
+
+app.get("/", (req, res) => {
+    res.send("EPIC SNAP SHOWDOWN");
+});
+
 
 // Define a simple /ping route
 app.get("/ping", (req, res) => {
